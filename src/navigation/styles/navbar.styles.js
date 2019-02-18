1c3248093aeadd2b71c278d1bfa63f4810fdaa64
Hasmik Kirakosyan <hasmikkirakosyan97@gmail.com>
2019-02-19 01:16:25 +0400

    Bug fix #90

---
+++ src/navigation/styles/navbar.styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: '#163D7D',
+  },
+  headerTitleStyle: {
+    fontSize: 16,
+    fontWeight: 'normal',
+  },
+  rightIconStyle: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    color: '#fff',
+    fontSize: 27,
+    paddingRight: 17
+  }
+});
