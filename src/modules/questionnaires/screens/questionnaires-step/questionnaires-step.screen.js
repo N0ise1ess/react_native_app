d83e0202da6ba08e6f48ce88e21793be7d8ea2d1
KirillKogtev <kirillkogtev95@gmail.com>
2019-03-13 21:31:16 +0300

    add style

--- src/modules/questionnaires/screens/questionnaires-step/questionnaires-step.screen.js
+++ src/modules/questionnaires/screens/questionnaires-step/questionnaires-step.screen.js
@@ -31,17 +31,19 @@ class InnerComponent extends React.Component {
       <Container style={styles.container}>
         <View style={styles.text_margin}>
           <Text style={[styles.text, styles.text__normal, styles.text__blue, styles.padding_top_20]}>Вопрос 1 из 1</Text>
-          <Text style={[styles.text, styles.text__normal, styles.text__bold, styles.padding_top_20]}>Lorem bla bla bla bla bla bla bla bla bla bla bla bla</Text>
-          <Text style={[styles.text, styles.text__small, styles.text__gray, styles.padding_top_20 ]}>{'Варианты ответов'.toUpperCase()}</Text>
+          <Text style={[styles.text, styles.text__normal, styles.text__bold, styles.padding_top_10]}>Lorem bla bla bla bla bla bla bla bla bla bla bla bla</Text>
+          <Text style={[styles.text, styles.text__small, styles.text__gray, styles.padding_top_10 ]}>{'Варианты ответов'.toUpperCase()}</Text>
         </View>
-        <ScrollView style={styles.padding_top_20}>
+        <ScrollView style={styles.padding_top_10}>
           {new Array(1, 2, 3, 4, 5).map((item, index) => <Card key={index} style={[styles.item_answer, styles.text_margin]}>
             <Text style={[styles.text, styles.text__normal, styles.text__bold]}>{item}</Text>
           </Card>)}
         </ScrollView>
-        <Button rounded style={[styles.button, styles.padding_20]}>
-          <Text>Готово</Text>
-        </Button>
+        <View style={[styles.button_container, styles.button_container__right, styles.padding_10, styles.margin_left__12]}>
+          <Button rounded style={[styles.button]}>
+            <Text>Готово</Text>
+          </Button>
+        </View>
         <FooterSection userStatus={userStatus} navigate={navigation.navigate} />
       </Container>
     )
--- src/modules/questionnaires/screens/questionnaires-step/styles/questionnaires-step.styles.js
+++ src/modules/questionnaires/screens/questionnaires-step/styles/questionnaires-step.styles.js
@@ -1,54 +1,68 @@
 import { StyleSheet, Dimensions } from 'react-native';
 import * as settingsFonts from '../../../../../constants/styles';
-import {getSizeFonts} from '../../../../shared/functions/styles';
+import { getSizeFonts } from '../../../../shared/functions/styles';
 
 export const styles = (fontSize) => StyleSheet.create({
-    container: {
-        backgroundColor: '#CED8DA',
-        flex: 1,
-        justifyContent: 'flex-end',
-    },
-    text_margin: {
-        paddingLeft: '12%',
-        paddingRight: 10,
-    },
-    text: {
-        fontFamily: 'MyriadPro-Regular',
-        color: '#000',
-    },
-    text__normal: {
-        fontSize: getSizeFonts(settingsFonts.FONT_SIZE_16, fontSize),
-    },
-    text__blue: {
-        color: '#496d9e',
-    },
-    text__bold: {
-        fontFamily: 'MyriadPro-Bold',
-    },
-    text__small: {
-        fontSize: getSizeFonts(settingsFonts.FONT_SIZE_10, fontSize),
-    },
-    text__gray: {
-        color: '#666'
-    },
-    padding_top_20: {
-        marginTop: 15,
-    },
-    padding_20: {
-        marginTop: 15,
-        marginBottom: 15,
-    },
-    item_answer: {
-        backgroundColor: '#fff',
-        paddingTop: 8,
-        paddingBottom: 8,
-    },
-    item_answer__active: {
-        backgroundColor: '#fff',
-    },
-    button: {
-        backgroundColor: '#ff003c',
-        width: 150,
-        justifyContent: 'center',
-    }
+	container: {
+		backgroundColor: '#CED8DA',
+		flex: 1,
+	},
+	text_margin: {
+		paddingLeft: '10%',
+		paddingRight: 10,
+	},
+	margin_left__12: {
+		paddingRight: '10%',
+	},
+	text: {
+		fontFamily: 'MyriadPro-Regular',
+		color: '#000',
+	},
+	text__normal: {
+		fontSize: getSizeFonts(settingsFonts.FONT_SIZE_16, fontSize),
+	},
+	text__blue: {
+		color: '#496d9e',
+	},
+	text__bold: {
+		fontFamily: 'MyriadPro-Bold',
+	},
+	text__small: {
+		fontSize: getSizeFonts(settingsFonts.FONT_SIZE_10, fontSize),
+	},
+	text__gray: {
+		color: '#666'
+	},
+	padding_top_10: {
+		marginTop: 10,
+	},
+	padding_top_20: {
+		marginTop: 20,
+	},
+	padding_10: {
+		marginTop: 10,
+		marginBottom: 10,
+	},
+	item_answer: {
+		backgroundColor: '#fff',
+		paddingTop: 8,
+		paddingBottom: 8,
+	},
+	item_answer__active: {
+		backgroundColor: '#fff',
+	},
+	button: {
+		backgroundColor: '#ff003c',
+		width: 150,
+		justifyContent: 'center',
+	},
+	button_container: {
+		// flex: 1,
+		width: '100%',
+		height: 50,
+		flexDirection: 'row',
+	},
+	button_container__right: {
+		justifyContent: 'flex-end',
+	},
 });
