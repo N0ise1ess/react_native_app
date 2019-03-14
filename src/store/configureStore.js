2cfaff497b18d798dc1f47e8bd5310b2c9ff995c
Vaagn Avanesyan <vahagn1993@gmail.com>
2019-03-14 22:07:21 +0300

    Revert persist

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,20 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import logger from 'redux-logger';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
-
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
+import logger from 'redux-logger';
 import rootReducer from '../reducers';
 
-let store = compose(applyMiddleware(thunk, logger))(createStore)(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['authReducer', 'loadingScreen', 'libraryReducer', 'newsReducer', 'financeReducer', 'timetableReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = compose(applyMiddleware(thunk, logger))(createStore)(persistedReducer);
+
+persistStore(store, () => {});
+
 export default store;
