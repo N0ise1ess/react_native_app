0cb13c3a8f606de2852180699ff80c35669bcb3f
KirillKogtev <kirillkogtev95@gmail.com>
2019-04-14 13:43:11 +0300

    add push ios

--- ios/campus/AppDelegate.h
+++ ios/campus/AppDelegate.h
@@ -6,8 +6,10 @@
  */
 
 #import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+@import UserNotifications;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
